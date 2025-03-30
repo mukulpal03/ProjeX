ed55837b97318dfeff837b0beab8767d8d2af9c9
Mukul Pal <mukulpal0333@gmail.com>
2025-03-30 14:01:04 +0530

    Project init

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import express from "express";
+import healthCheckRouter from "./routes/healthcheck.routes.js"
+const app = express();
+
+app.use('/api/v1/healthcheck', healthCheckRouter)
+
+export default app;
